1d430f15185c744eb8f10d1f0b57af480891b502
Iryna Yakymchuk <iryna.work.it@gmail.com>
2024-05-03 07:46:47 +0200

    first commit

---
+++ src/main/java/de/ait/take_me_home/TakeMeHomeApplication.java
@@ -0,0 +1,13 @@
+package de.ait.take_me_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeMeHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeMeHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ait/take_me_home/TakeMeHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ait.take_me_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeMeHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
